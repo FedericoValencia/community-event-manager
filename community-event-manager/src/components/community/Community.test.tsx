b564f84711f4e75360d9a036de7259d969c1559a
federico.valencia <federico.valencia@federicovalencias-MacBook-Pro.local>
2023-04-21 11:28:44 +0200

    [FV,SH] - Refactor community test

--- community-event-manager/src/components/community/Community.test.tsx
+++ community-event-manager/src/components/community/Community.test.tsx
@@ -1,41 +1,19 @@
 import {render, screen} from "@testing-library/react";
 import Community from "./Community";
-import {MemoryRouter, Route, Routes} from "react-router-dom";
 import React from "react";
 import {act} from "react-dom/test-utils";
 
 jest.mock('react-router-dom', () => ({
-    ...jest.requireActual('react-router-dom'), 
+    ...jest.requireActual('react-router-dom'),
     useParams: () => ({
-      id: 'data',
+        id: 'data',
     }),
-    useRouteMatch: () => ({ url: '/communities/data' }),
-  }));
+    useRouteMatch: () => ({url: '/communities/data'}),
+}));
 
 describe('Community in detail component', () => {
 
-    it('should render Community title', async () => {
-        const expectedCommunity = {
-            name: 'Data Community',
-        };
-        const mockedCommunitiesAPI = jest.fn(() =>
-            Promise.resolve({
-                json: () => Promise.resolve(expectedCommunity),
-            })
-        ) as jest.Mock
-        const fetchMock = jest.spyOn(global, "fetch").mockImplementation(mockedCommunitiesAPI);
-
-        await act(async () => {
-            render(<Community/>);
-        });
-
-        const title = screen.getByText(/Data Community/i);
-        expect(title).toBeInTheDocument();
-        expect(fetchMock).toHaveBeenCalledWith('http://localhost:3001/api/communities/data');
-        fetchMock.mockRestore();
-    });
-
-    it('should render the Community events', async () => {
+    it('should render title and  the Community events', async () => {
         const expectedCommunity = {
             name: 'Data Community',
             events: ['Event 1', 'Event 2']
@@ -45,12 +23,14 @@ describe('Community in detail component', () => {
                 json: () => Promise.resolve(expectedCommunity),
             })
         ) as jest.Mock
-        const fetchMock = jest.spyOn(global, "fetch").mockImplementation(mockedCommunitiesAPI);
+        jest.spyOn(global, "fetch").mockImplementation(mockedCommunitiesAPI);
 
         await act(async () => {
             render(<Community/>);
         });
 
+        const title = screen.getByText("Data Community");
+        expect(title).toBeInTheDocument()
         const [event1, event2] = screen.getAllByRole('listitem');
         expect(event1).toHaveTextContent("Event 1")
         expect(event2).toHaveTextContent("Event 2")
